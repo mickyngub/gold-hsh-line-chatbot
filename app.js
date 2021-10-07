f8e2ca197e67a78e3f4f1c0deaf3d813ea14e3af
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-10-07 10:48:00 +0700

    added axios for pinging

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 4000;
 
